b244cf8d20988e2c09c71c3066275ed9ee33342e
cauzinc <514322058@qq.com>
2019-04-27 21:00:10 +0800

    update: common response body class

---
+++ project/myMail/src/main/java/com/myMall/common/ResponseCode.java
@@ -0,0 +1,25 @@
+package com.myMall.common;
+
+// 用枚举类定义返回时的状态码和信息，避免写在各个具体实现中，防止硬编码。
+public enum ResponseCode {
+    SUCCESS(0, "SUCCESS"),
+    ERROR(1, "ERROR"),
+    NEED_LOGIN(10, "NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2, "ILLEGAL_ARGUMENT");
+
+    private final int code;
+    private final String desc;
+
+    ResponseCode(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
---
+++ project/myMail/src/main/java/com/myMall/common/ServerResponse.java
@@ -0,0 +1,86 @@
+package com.myMall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+// 当有数据为null的时候，序列化时忽略这个数据
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class ServerResponse<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status) {
+        this.status = status;
+    }
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+    private ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    //  增加注解，反序列化后，不要让这个字段显示在json中
+    @JsonIgnore
+    public boolean isSuccess() {
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    // 成功的响应
+    public static <T> ServerResponse<T> createBySuccess() {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessByMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    // 失败的响应
+    public static <T> ServerResponse<T> createByError() {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode());
+    }
+
+    public static <T> ServerResponse<T> createByErrorByMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), msg);
+    }
+
+    public static <T> ServerResponse<T> createByErrorByErrorCode(int errorCode, String msg) {
+        return new ServerResponse<T>(errorCode, msg);
+    }
+
+    //    public static void main(String[] args) {
+//        // 测试两个构造器会不会导致混淆
+//        ServerResponse sr1 = new ServerResponse(0, new Object());
+//        ServerResponse sr2 = new ServerResponse(0, "test");
+//        System.out.println(sr2.msg);
+//    }
+
+}
