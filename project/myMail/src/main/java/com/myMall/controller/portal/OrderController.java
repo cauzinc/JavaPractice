2a8dd55c57014ba545317972863a98896f816202
cauzinc <514322058@qq.com>
2019-05-08 18:18:04 +0800

    update: pay order functional structure

--- project/myMail/src/main/java/com/myMall/controller/portal/OrderController.java
+++ project/myMail/src/main/java/com/myMall/controller/portal/OrderController.java
@@ -1,6 +1,8 @@
 package com.myMall.controller.portal;
 
 import com.myMall.common.ServerResponse;
+import com.myMall.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +12,20 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/order/")
 public class OrderController {
+    private IOrderService iOrderService;
+    @Autowired
+    public void setiOrderService(IOrderService iOrderService) {
+        this.iOrderService = iOrderService;
+    }
 
+    /**
+     * 生成订单二维码和二维码信息并返回到前端
+      * @param session
+     * @param orderNo
+     * @param request
+     * @return Map<String, String> 订单号，二维码地址
+     */
     public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request) {
-
         // 获取运行时项目的真实路径
         String path = request.getSession().getServletContext().getRealPath("upload");
 
--- project/myMail/src/main/java/com/myMall/service/impl/OrderService.java
+++ project/myMail/src/main/java/com/myMall/service/impl/OrderService.java
@@ -1,10 +1,87 @@
 package com.myMall.service.impl;
 
+import com.alipay.api.internal.util.AlipayUtils;
+import com.alipay.api.response.AlipayTradePrecreateResponse;
+import com.alipay.demo.trade.model.result.AlipayF2FPrecreateResult;
+import com.google.common.collect.Maps;
+import com.myMall.common.ServerResponse;
+import com.myMall.dao.OrderItemMapper;
+import com.myMall.dao.OrderMapper;
+import com.myMall.pojo.Order;
+import com.myMall.pojo.OrderItem;
 import com.myMall.service.IOrderService;
+import com.myMall.util.AlipayUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service("iOrderService")
 public class OrderService implements IOrderService {
+    private OrderMapper orderMapper;
+    private OrderItemMapper orderItemMapper;
+    private static Logger logger = LoggerFactory.getLogger(OrderService.class);
+    @Autowired
+    public void setOrderMapper(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
+    @Autowired
+    public void setOrderItemMapper(OrderItemMapper orderItemMapper) {
+        this.orderItemMapper = orderItemMapper;
+    }
 
+    public ServerResponse pay(Integer userId, Long orderNo, String filePath) {
+        Map<String, String> resultMap = Maps.newHashMap();
+
+        // todo 获取订单信息，检查订单是否存在
+        Order order = orderMapper.selectByUserIdAndOrderNo(userId, orderNo);
+        if(order == null) {
+            return ServerResponse.createBySuccessByMessage("订单不存在");
+        }
+        resultMap.put("orderNo", order.getOrderNo().toString());
+
+        // todo 调用支付宝接口, 获取response
+        List<OrderItem> orderItemList = orderItemMapper.getByOrderNoUserId(orderNo, userId);
+        AlipayF2FPrecreateResult result = AlipayUtil.createQrCode(order, orderItemList);
+
+        String errorMsg = "";
+        switch (result.getTradeStatus()) {
+            case SUCCESS:
+
+                logger.info("支付宝预下单成功: )");
+
+                AlipayTradePrecreateResponse response = result.getResponse();
+                AlipayUtil.dumpResponse(response);
+
+                // todo 把二维码图片上传到自己的ftp服务器
+
+                // 需要修改为运行机器上的路径
+//                String filePath = String.format("/Users/sudo/Desktop/qr-%s.png",
+//                        response.getOutTradeNo());
+//                logger.info("filePath:" + filePath);
+                //                ZxingUtils.getQRCodeImge(response.getQrCode(), 256, filePath);
+
+                
+                return ServerResponse.createBySuccess(resultMap);
+
+            case FAILED:
+                errorMsg = "支付宝预下单失败!!!";
+                break;
+
+            case UNKNOWN:
+                errorMsg = "系统异常，预下单状态未知!!!";
+                break;
+
+            default:
+                errorMsg = "不支持的交易状态，交易返回异常!!!";
+                break;
+        }
+
+        logger.error(errorMsg);
+        return ServerResponse.createByErrorByMessage(errorMsg);
+    }
 
 }
