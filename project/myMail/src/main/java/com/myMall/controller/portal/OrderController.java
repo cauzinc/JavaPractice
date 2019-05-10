d9ec5bbf71b778381d0c50d1974477d71d33b8f3
cauzinc <514322058@qq.com>
2019-05-10 20:52:41 +0800

    fix: complete controller

--- project/myMail/src/main/java/com/myMall/controller/portal/OrderController.java
+++ project/myMail/src/main/java/com/myMall/controller/portal/OrderController.java
@@ -1,10 +1,14 @@
 package com.myMall.controller.portal;
 
+import com.myMall.common.Const;
 import com.myMall.common.ServerResponse;
+import com.myMall.pojo.User;
 import com.myMall.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,12 +29,15 @@ public class OrderController {
      * @param request
      * @return Map<String, String> 订单号，二维码地址
      */
+    @ResponseBody
+    @RequestMapping(value = "pay.do", method = RequestMethod.POST)
     public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request) {
         // 获取运行时项目的真实路径
         String path = request.getSession().getServletContext().getRealPath("upload");
-
-        return null;
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        return iOrderService.pay(user.getId(), orderNo, path);
     }
+
 
 
 
--- project/myMail/src/main/java/com/myMall/service/IOrderService.java
+++ project/myMail/src/main/java/com/myMall/service/IOrderService.java
@@ -1,4 +1,7 @@
 package com.myMall.service;
 
+import com.myMall.common.ServerResponse;
+
 public interface IOrderService {
+    ServerResponse pay(Integer userId, Long orderNo, String path);
 }
