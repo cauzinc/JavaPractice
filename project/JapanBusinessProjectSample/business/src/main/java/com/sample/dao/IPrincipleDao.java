d93d3a2835092716d5b246241e8cde582966ca87
cauzinc <514322058@qq.com>
2019-05-11 20:31:56 +0800

    fix: refractor file name

--- project/JapanBusinessProjectSample/business/src/main/java/com/sample/dao/IPrincipleDao.java
+++ project/JapanBusinessProjectSample/business/src/main/java/com/sample/dao/IPrincipleDao.java
@@ -1,4 +1,14 @@
 package com.sample.dao;
 
+import com.sample.pojo.Principle;
+import com.sample.vo.PrincipleListItem;
+
+import java.util.List;
+
 public interface IPrincipleDao {
+    List<PrincipleListItem> getPrincipleRelatedList();
+    void insertPrinciple(Principle principle);
+    void updatePrinciple(Principle principle);
+    void deletePrinciple(Integer id);
+    Principle getPrincipleById(Integer id);
 }
