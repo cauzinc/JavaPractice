6e783d466baca813af39da231221eac4dca08a7e
cauzinc <514322058@qq.com>
2019-05-11 20:24:25 +0800

    update: principle related api

--- project/JapanBusinessProjectSample/business/src/main/java/com/sample/vo/PrincipleListItem.java
+++ project/JapanBusinessProjectSample/business/src/main/java/com/sample/vo/PrincipleListItem.java
@@ -4,4 +4,28 @@ public class PrincipleListItem {
     private String principleName;
     private String businessName;
     private String tel;
+
+    public String getPrincipleName() {
+        return principleName;
+    }
+
+    public void setPrincipleName(String principleName) {
+        this.principleName = principleName;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 }
