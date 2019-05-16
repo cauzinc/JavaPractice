41aad7809d460edf464e7f8223d6801df8ed9f44
cauzinc <514322058@qq.com>
2019-05-16 23:16:37 +0800

    update: scan beans into spring context

--- project/JapanBusinessProjectSample/business/src/main/java/com/sample/controller/BusinessController.java
+++ project/JapanBusinessProjectSample/business/src/main/java/com/sample/controller/BusinessController.java
@@ -5,6 +5,7 @@ import com.sample.common.ResponseCode;
 import com.sample.common.ServerResponse;
 import com.sample.dao.IBusinessDao;
 import com.sample.dao.daoImplement.BusinessDaoImpl;
+import com.sample.daoMapper.BusinessMapper;
 import com.sample.pojo.Business;
 import com.sample.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 public class BusinessController {
     private IBusinessDao iBusinessDao;
     private BusinessService businessService;
+    private BusinessMapper businessMapper;
 
     @Autowired
     public void setBusinessService(BusinessService businessService) {
@@ -26,6 +28,10 @@ public class BusinessController {
     @Autowired
     public void setiBusinessDao(IBusinessDao iBusinessDao) {
         this.iBusinessDao = iBusinessDao;
+    }
+    @Autowired
+    public void setBusinessMapper(BusinessMapper businessMapper) {
+        this.businessMapper = businessMapper;
     }
 
     @ResponseBody
