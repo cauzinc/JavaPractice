e346d86bf9e96fe0ccb44ed7f124375a7940b26b
cauzinc <514322058@qq.com>
2019-05-17 16:07:47 +0800

    update: make businessList into vuex

--- project/JapanBusinessProjectSample/business/src/main/webapp/src/store/modules/business.js
+++ project/JapanBusinessProjectSample/business/src/main/webapp/src/store/modules/business.js
@@ -2,16 +2,39 @@ import Service from './../Service'
 
 let service = new Service({
     prefix: "/api/business/",
-    state: {},
+    state: {
+        businessList: [
+            { value1: "", value2: "", value3: "" },
+            { value1: "", value2: "", value3: "" },
+            { value1: "", value2: "", value3: "" },
+            { value1: "", value2: "", value3: "" },
+            { value1: "", value2: "", value3: "" },
+            { value1: "", value2: "", value3: "" }
+        ]
+    },
     actions: {
         getBusinessList({ commit }, data) {
-            return service.fetch('getBusinessList', 'get', data);
+            // return service.fetch('getBusinessList', 'get', data);
+            service.fetch('getBusinessList', 'get', data).then(res => {
+                if(res.data && res.data.status === 0) {
+                    commit('FETCH_BUSINESS_LIST', res.data && res.data.data);
+                }
+            })
         },
         addToCart({ commit }, data) {
             return service.fetch('addToCart', 'post', data);
         }
     },
-    mutations: {}
+    mutations: {
+        FETCH_BUSINESS_LIST(state, data) {
+            data.map((item, index) => {
+                state.businessList[index].value1 = item.businessName;
+                state.businessList[index].value2 = item.address;
+                state.businessList[index].value3 = item.businessTel;
+            //    state.businessList[index].id = item.id;
+            })
+        }
+    }
 });
 
 export default service
