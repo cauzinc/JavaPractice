229991d7be7b33667dac06fb693832f572ae1421
cauzinc <514322058@qq.com>
2019-05-19 01:19:40 +0800

    update: insert new business

--- project/JapanBusinessProjectSample/business/src/main/webapp/src/route/entry.js
+++ project/JapanBusinessProjectSample/business/src/main/webapp/src/route/entry.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import BusinessHome from '../pages/business/BusinessHome'
 import BusinessDetail from '../pages/business/BusinessDetail'
-import App from '../App'
+import BusinessCreate from '../pages/business/BusinessCreate'
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         { path: '/', name: 'businessHome', component: BusinessHome },
-        { path: '/business-detail', name: "businessDetail", component: BusinessDetail }
+        { path: '/business-detail', name: "businessDetail", component: BusinessDetail },
+        { path: '/business-create', name: "businessCreate", component: BusinessCreate }
     ]
 })
